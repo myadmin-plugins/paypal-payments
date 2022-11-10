1932548f7d94e6f4e744a2c882c7a208b76ae0f6
Joe Huss <detain@interserver.net>
2022-11-10 12:29:32 -0500

    updates changing some of the settings to use the new password type

--- src/Plugin.php
+++ src/Plugin.php
@@ -95,13 +95,13 @@ class Plugin
         $settings->add_radio_setting(_('Billing'), _('PayPal'), 'paypal_enable', _('Enable PayPal'), _('Enable PayPal'), PAYPAL_ENABLE, [true, false], ['Enabled', 'Disabled']);
         $settings->add_radio_setting(_('Billing'), _('PayPal'), 'paypal_digitalgoods_enable', _('Enable Digital Goods'), _('Enable Digital Goods'), PAYPAL_DIGITALGOODS_ENABLE, [true, false], ['Enabled', 'Disabled']);
         $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_email', _('Login Email'), _('Login Email'), (defined('PAYPAL_EMAIL') ? PAYPAL_EMAIL : ''));
-        $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_client_id', _('Client ID'), _('Client ID'), (defined('PAYPAL_CLIENT_ID') ? PAYPAL_CLIENT_ID : ''));
-        $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_secret', _('Secret'), _('SecretID'), (defined('PAYPAL_SECRET') ? PAYPAL_SECRET : ''));
+        $settings->add_password_setting(_('Billing'), _('PayPal'), 'paypal_client_id', _('Client ID'), _('Client ID'), (defined('PAYPAL_CLIENT_ID') ? PAYPAL_CLIENT_ID : ''));
+        $settings->add_password_setting(_('Billing'), _('PayPal'), 'paypal_secret', _('Secret'), _('SecretID'), (defined('PAYPAL_SECRET') ? PAYPAL_SECRET : ''));
         $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_api_username', _('API Username'), _('API Username'), (defined('PAYPAL_API_USERNAME') ? PAYPAL_API_USERNAME : ''));
-        $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_api_password', _('API Password'), _('API Password'), (defined('PAYPAL_API_PASSWORD') ? PAYPAL_API_PASSWORD : ''));
-        $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_api_signature', _('API Signature'), _('API Signature'), (defined('PAYPAL_API_SIGNATURE') ? PAYPAL_API_SIGNATURE : ''));
+        $settings->add_password_setting(_('Billing'), _('PayPal'), 'paypal_api_password', _('API Password'), _('API Password'), (defined('PAYPAL_API_PASSWORD') ? PAYPAL_API_PASSWORD : ''));
+        $settings->add_password_setting(_('Billing'), _('PayPal'), 'paypal_api_signature', _('API Signature'), _('API Signature'), (defined('PAYPAL_API_SIGNATURE') ? PAYPAL_API_SIGNATURE : ''));
         $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_sandbox_api_username', _('Sandbox API Username'), _('Sandbox API Username'), (defined('PAYPAL_SANDBOX_API_USERNAME') ? PAYPAL_SANDBOX_API_USERNAME : ''));
-        $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_sandbox_api_password', _('Sandbox API Password'), _('Sandbox API Password'), (defined('PAYPAL_SANDBOX_API_PASSWORD') ? PAYPAL_SANDBOX_API_PASSWORD : ''));
-        $settings->add_text_setting(_('Billing'), _('PayPal'), 'paypal_sandbox_api_signature', _('Sandbox API Signature'), _('Sandbox API Signature'), (defined('PAYPAL_SANDBOX_API_SIGNATURE') ? PAYPAL_SANDBOX_API_SIGNATURE : ''));
+        $settings->add_password_setting(_('Billing'), _('PayPal'), 'paypal_sandbox_api_password', _('Sandbox API Password'), _('Sandbox API Password'), (defined('PAYPAL_SANDBOX_API_PASSWORD') ? PAYPAL_SANDBOX_API_PASSWORD : ''));
+        $settings->add_password_setting(_('Billing'), _('PayPal'), 'paypal_sandbox_api_signature', _('Sandbox API Signature'), _('Sandbox API Signature'), (defined('PAYPAL_SANDBOX_API_SIGNATURE') ? PAYPAL_SANDBOX_API_SIGNATURE : ''));
     }
 }
