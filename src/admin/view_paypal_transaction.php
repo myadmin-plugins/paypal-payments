c02414429acf6ee5e070ac89ab6bc0403417928d
Joe Huss <detain@interserver.net>
2024-12-05 02:37:46 -0500

    update to plugins

--- src/admin/view_paypal_transaction.php
+++ src/admin/view_paypal_transaction.php
@@ -3,7 +3,7 @@
     /**
      * PayPal Related Functionality
      * @author Joe Huss <detain@interserver.net>
-     * @copyright 2019
+     * @copyright 2025
      * @package MyAdmin
      * @category PayPal
      */
